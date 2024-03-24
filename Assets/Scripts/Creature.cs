a4ee5fb098f93155e8d6940df7c48cbabd7759f5
ChrisPrice03 <squacknog@gmail.com>
2024-03-24 18:18:41 -0400

    Added Creature Wandering

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -10,18 +10,38 @@ public class Creature : MonoBehaviour
 
     public HealthBar healthBar;
 
+    //for creature wandering
+    public float moveSpeed = 1f;
+    public float wanderInterval = 5f;
+
+    private bool isMoving = true;
+    private float timer;
+    private int direction;
+
     // Start is called before the first frame update
     void Start()
     {
         currentHealth = maxHealth;
         healthBar.setMaxHealth(maxHealth);
         healthBar.setHealth(currentHealth);
+        timer = wanderInterval;
+        direction = Random.Range(0, 2) == 0 ? -1 : 1;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer -= Time.deltaTime;
 
+        if (timer <= 0) {
+            ChangeDirection();
+            timer = wanderInterval;
+        }
+
+        // Move in the chosen direction
+        if (isMoving) {
+            transform.Translate(Vector2.right * direction * moveSpeed * Time.deltaTime);
+        }
     }
 
     //allows the creature to take damage
@@ -37,8 +57,16 @@ public class Creature : MonoBehaviour
         }
     }
 
+    //kills creature
     void Die() {
         GetComponent<Collider2D>().enabled = false;
         this.enabled = false;
+    }
+
+    //randomly sets wander direction
+    void ChangeDirection() {
+        // Randomly choose left or right
+        direction = Random.Range(0, 2) == 0 ? -1 : 1;
+        isMoving = Random.Range(0, 2) == 0 ? false : true;
     }
 }
