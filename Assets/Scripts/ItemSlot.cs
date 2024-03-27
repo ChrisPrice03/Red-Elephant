d49436990cf8f2dbea165cbc801d5fac5c57dfd4
Ifeogunbanjo <iogunban@purdue.edu>
2024-03-27 10:19:14 -0400

    Inventory
    Stackable Items

--- Assets/Scripts/ItemSlot.cs
+++ Assets/Scripts/ItemSlot.cs
@@ -5,7 +5,7 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ItemSlot : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     //======ITEM DATA===//
     public string itemName;
@@ -13,7 +13,10 @@ public class ItemSlot : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
     public Sprite itemSprite;
     public bool isFull;
     public string itemDescription;
+    public Sprite emptySprite;
 
+    [SerializeField]
+    private int maxNumberOfItems;
     //===ITEM SLOT===//
     [SerializeField]
     private TMP_Text quantityText;
@@ -35,26 +38,37 @@ public class ItemSlot : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
         inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
     }
 
-    public void AddItem(string itemName, int quantity, Sprite itemSprite, string itemDescription)
+    public int AddItem(string itemName, int quantity, Sprite itemSprite, string itemDescription)
     {
+        if (isFull)
+        return quantity;
+
         this.itemName = itemName;
-        this.quantity = quantity;
+        
         this.itemSprite = itemSprite;
-        this.itemDescription = itemDescription;
-        isFull = true;
-
-        quantityText.text = quantity.ToString();
-        quantityText.enabled = true;
         itemImage.sprite = itemSprite;
+        this.itemDescription = itemDescription;
+
+        this.quantity += quantity;
+        if (this.quantity >= maxNumberOfItems)
+        {
+            quantityText.text = maxNumberOfItems.ToString();
+            quantityText.enabled = true;
+            isFull = true;
+        
+
+        int extraItems = this.quantity - maxNumberOfItems;
+        this.quantity = maxNumberOfItems;
+        return extraItems;
+        }
+
+        quantityText.text = this.quantity.ToString();
+        quantityText.enabled = true;
+
+        return 0;
     }
 
-    public void OnPointerDown( PointerEventData eventData )
-    {
-    }
-
-    public void OnPointerUp( PointerEventData eventData )
-    {
-    }
+    
     public void OnPointerClick(PointerEventData eventData)
 
     {
@@ -76,6 +90,10 @@ public class ItemSlot : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
         ItemDescriptionNameText.text = itemName;
         ItemDescriptionText.text = itemDescription;
         itemDescriptionImage.sprite = itemSprite;
+        if(itemDescriptionImage.sprite == null)
+        {
+            itemDescriptionImage.sprite = emptySprite;
+        }
     }
 
     public void OnRightClick()
