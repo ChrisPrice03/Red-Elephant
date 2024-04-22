907fef8098bb33de698b5eaa9385a6fda36202df
Ifeogunbanjo <iogunban@purdue.edu>
2024-04-22 15:07:09 -0400

    Leaderboard & Sound

--- Assets/Scenes/Something.cs
+++ Assets/Scenes/Something.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreBoard : MonoBehaviour
+public class Something : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
